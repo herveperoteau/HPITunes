6690c0dbaf94a192090ef0d8206e7c8b4520c856
Herve Peroteau <herve.peroteau@gmail.com>
2014-04-07 10:02:06 +0200

    Initial Commit

---
+++ HPITunes/HPITunes.h
@@ -0,0 +1,13 @@
+//
+//  HPITunes.h
+//  HPITunes
+//
+//  Created by Hervé PEROTEAU on 07/04/2014.
+//  Copyright (c) 2014 Hervé PEROTEAU. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HPITunes : NSObject
+
+@end
---
+++ HPITunes/HPITunes.m
@@ -0,0 +1,13 @@
+//
+//  HPITunes.m
+//  HPITunes
+//
+//  Created by Hervé PEROTEAU on 07/04/2014.
+//  Copyright (c) 2014 Hervé PEROTEAU. All rights reserved.
+//
+
+#import "HPITunes.h"
+
+@implementation HPITunes
+
+@end
